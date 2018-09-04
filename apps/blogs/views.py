e6d772c59cf9f92976f793be7c8b2c173f1b85c6
Zachkery <zachkery999@gmail.com>
2018-09-04 11:38:35 -0500

    Created required files
    Created/added all required files to run this django app.
    Added regex needed for blogs.urls with a method in views to execute for each recognized pattern.
    Added all methods/functions in blogs.views to practice routing and set up

---
+++ apps/blogs/views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    response = "Placeholder to list all blogs"
+    return HttpResponse(response)
+
+def new(request):
+    response = 'Placeholder for a form to create a new blog'
+    return HttpResponse(response)
+
+def create(request):
+    return redirect('/')
+
+def show(request, number):
+    response = 'Placeholder to display '+str(number)
+    return HttpResponse(response)
+
+def edit(request, number):
+    response = 'Placeholder to edit blog '+str(number)
+    return HttpResponse(response)
+
+def destroy(request, number):
+    return redirect('/')
